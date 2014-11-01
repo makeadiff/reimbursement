72f20e391454bd8e40da57350b922aff7a82c147
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2014-11-01 20:41:09 +0530

    Reimbursement closed error

--- app/routes.php
+++ app/routes.php
@@ -17,17 +17,24 @@ Route::filter('login_check',function()
    session_start();
 
    if(empty($_SESSION['user_id'])){
-       return Redirect::to('http://makeadiff.in/madapp/index.php/auth/login/' . base64_encode(Request::url()));
-       //return Redirect::to('http://localhost/makeadiff.in/home/makeadiff/public_html/apps/set_session_test.php?url=' . base64_encode(Request::url()));
+      return Redirect::to('http://makeadiff.in/madapp/index.php/auth/login/' . base64_encode(Request::url()));
+      //return Redirect::to('http://localhost/makeadiff.in/home/makeadiff/public_html/apps/set_session_test.php?url=' . base64_encode(Request::url()));
    }
 
 
 });
 
+Route::filter('reimbursement_active',function()
+    {
+        return Redirect::to('/error')->with('message','Reimbursements for the month of July are currently being processed nationally. You can apply for reimbursements once it has been closed.');
+
+    });
+
+
 Route::group(array('before'=>'login_check'),function()
 {
     Route::get('/', 'Reimbursement@showHome');
-    Route::get('/telephone-internet','Reimbursement@showTelephoneInternet');
+    Route::get('/telephone-internet',array('uses' => 'Reimbursement@showTelephoneInternet', 'before'=>'reimbursement_active'));
     Route::get('/travel','Reimbursement@showTravel');
     Route::get('/success','Reimbursement@showSuccess');
     Route::get('/error','Reimbursement@showError');
