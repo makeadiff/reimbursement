559a15d241c0c4b8b0318a40099e5625d79649f3
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2014-12-31 20:06:21 +0530

    Added month Sep to Dec

--- app/views/telephone-internet.blade.php
+++ app/views/telephone-internet.blade.php
@@ -36,7 +36,10 @@
 
         <h4 class="sub-title">Month</h4>
         <select id="monthSelect" class="form-control" placeholder="Mode" name="monthSelect">
-            <option>August</option>
+            <option>September</option>
+            <option>October</option>
+            <option>November</option>
+            <option>December</option>
         </select>
         </div></div>
           <div class="row"><div class="form-group col-md-4 col-md-offset-4 col-sm-12">
