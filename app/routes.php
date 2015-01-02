3e95c767125ae02ed9e0c4d7d6ca2c6ca4d460b5
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2015-01-02 10:12:08 +0530

    Removed reimbursement processing message

--- app/routes.php
+++ app/routes.php
@@ -34,7 +34,7 @@ Route::filter('reimbursement_active',function()
 Route::group(array('before'=>'login_check'),function()
 {
     Route::get('/', 'Reimbursement@showHome');
-    Route::get('/telephone-internet',array('uses' => 'Reimbursement@showTelephoneInternet','before' => 'reimbursement_active'));
+    Route::get('/telephone-internet',array('uses' => 'Reimbursement@showTelephoneInternet','before' => ''));
     Route::get('/travel','Reimbursement@showTravel');
     Route::get('/success','Reimbursement@showSuccess');
     Route::get('/error','Reimbursement@showError');
