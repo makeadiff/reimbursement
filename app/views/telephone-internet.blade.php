605540e16939fa69ef013ce5865e16f949e1fb72
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2016-07-26 14:43:51 +0530

    July

--- app/views/telephone-internet.blade.php
+++ app/views/telephone-internet.blade.php
@@ -41,7 +41,7 @@
                     <div class="row">
                         <div class="form-group col-md-6 col-sm-12">
                             <select id="monthSelect" class="form-control" placeholder="Month" name="monthSelect">
-                                <option selected>May</option>
+                                <option selected>July</option>
                             </select>s 
                         </div>
                         <div class="form-group col-md-6 col-sm-12">
