4e0ca064f57113e145b5c2eca9da9bd0e4bd38f6
Binny V A <binnyva@gmail.com>
2022-02-04 16:17:31 +0530

    Year change

--- app/views/telephone-internet.blade.php
+++ app/views/telephone-internet.blade.php
@@ -45,7 +45,7 @@
 
                             <div class="form-group col-md-6 col-sm-12">
                                 <select id="yearSelect" class="form-control" placeholder="Year" name="yearSelect">
-                                    <option selected>2021</option>
+                                    <option selected>2022</option>
                                 </select>
                             </div>
                         </div>
